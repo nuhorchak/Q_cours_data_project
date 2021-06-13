6158746771afe74a7e25c917e402e4b47bbbee9a
nuhorchak <nuhorchak@gmail.com>
2021-06-13 15:42:10 -0400

    working plotting function for city, not for travel method

--- data_pull.R
+++ data_pull.R
@@ -6,8 +6,18 @@ library(RSocrata)
 data_pull <- read.socrata("https://data.bts.gov/resource/keg4-3bc2.json")
 data_pull <- data_pull %>% mutate(., value = as.numeric(value))
 
-data_pull %>% select(-port_code) %>%
-  filter(port_name == 'Alcan') %>%
+data_pull %>% 
+  filter(port_name == 'Brownsville') %>%
+  filter(measure == ) %>% 
   ggplot(., aes(date, value, color=measure, linetype=measure)) + geom_line() +
   theme(axis.text.x = element_text(angle = 90))
+
+
+plot_port_city <- function(data, city = 'Brownsville', travel_method = , ...){
+  data %>% 
+    filter(port_name == city) %>%
+    filter(measure==travel_method) %>% 
+    ggplot(., aes(date, value, color=measure, linetype=measure)) + geom_line() +
+    theme(axis.text.x = element_text(angle = 90))
+}
 
