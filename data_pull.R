bd65fc36fd2105a742eecc58f6d3673e6dbade55
Nick Uhorchak <nick.uhorchak@sofcdo.com>
2021-06-12 20:58:38 -0400

    plot not working yet

--- data_pull.R
+++ data_pull.R
@@ -5,10 +5,10 @@ library(RSocrata)
 data_pull <- read.socrata("https://data.bts.gov/resource/keg4-3bc2.json")
 data_pull <- data_pull %>% mutate(., year = year(date)) %>% mutate(., month = month(date))
 
-data_pull %>% select(-port_code) %>% 
-  filter(measure == 'Trucks') %>%
-  filter(border=="US-Canada Border") %>% 
-  group_by(state, date, value, port_name) %>% 
-  summarise() %>% filter(state=='Alaska') %>% 
-  ggplot(., aes(month, value, color=port_name, linetype=port_name)) + geom_line() +
-  theme(axis.text.x = element_text(angle = 90))
+# data_pull %>% select(-port_code) %>% 
+#   filter(measure == 'Trucks') %>%
+#   filter(border=="US-Canada Border") %>% 
+#   group_by(state, date, value, port_name) %>% 
+#   summarise() %>% filter(state=='Alaska') %>% 
+#   ggplot(., aes(month, value, color=port_name, linetype=port_name)) + geom_line() +
+#   theme(axis.text.x = element_text(angle = 90))
