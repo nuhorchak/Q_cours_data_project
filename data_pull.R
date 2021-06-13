4c0702d659e09aa4ac05235c5acad644307c235b
nuhorchak <nuhorchak@gmail.com>
2021-06-13 11:54:16 -0400

    update value from factor to numeric, working graph

--- data_pull.R
+++ data_pull.R
@@ -1,14 +1,13 @@
 library(tidyverse)
+library(lubridate)
 library(forecast)
 library(RSocrata)
 
 data_pull <- read.socrata("https://data.bts.gov/resource/keg4-3bc2.json")
-data_pull <- data_pull %>% mutate(., year = year(date)) %>% mutate(., month = month(date))
+data_pull <- data_pull %>% mutate(., value = as.numeric(value))
 
-# data_pull %>% select(-port_code) %>% 
-#   filter(measure == 'Trucks') %>%
-#   filter(border=="US-Canada Border") %>% 
-#   group_by(state, date, value, port_name) %>% 
-#   summarise() %>% filter(state=='Alaska') %>% 
-#   ggplot(., aes(month, value, color=port_name, linetype=port_name)) + geom_line() +
-#   theme(axis.text.x = element_text(angle = 90))
+data_pull %>% select(-port_code) %>%
+  filter(port_name == 'Alcan') %>%
+  ggplot(., aes(date, value, color=measure, linetype=measure)) + geom_line() +
+  theme(axis.text.x = element_text(angle = 90))
+
