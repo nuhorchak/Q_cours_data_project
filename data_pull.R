1ffe2516556b7333adbcaf53a0da6c0c1924397c
nuhorchak <nuhorchak@gmail.com>
2021-06-13 18:05:37 -0400

    remove method from graph function

--- data_pull.R
+++ data_pull.R
@@ -5,18 +5,17 @@ library(RSocrata)
 
 data_pull <- read.socrata("https://data.bts.gov/resource/keg4-3bc2.json")
 data_pull <- data_pull %>% mutate(., value = as.numeric(value))
+data_pull <- data_pull %>% mutate(., measure = as.factor(measure))
 
 data_pull %>% 
   filter(port_name == 'Brownsville') %>%
-  filter(measure == ) %>% 
   ggplot(., aes(date, value, color=measure, linetype=measure)) + geom_line() +
   theme(axis.text.x = element_text(angle = 90))
 
 
-plot_port_city <- function(data, city = 'Brownsville', travel_method = 'Truck', ...){
+plot_port_city <- function(data, city = 'Brownsville', ...){
   data %>% 
     filter(port_name == city) %>%
-    filter(measure==travel_method) %>% 
     ggplot(., aes(date, value, color=measure, linetype=measure)) + geom_line() +
     theme(axis.text.x = element_text(angle = 90))
 }
