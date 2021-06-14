e8225a3bf4958075a4bc306c06e1d94b4df801db
nuhorchak <nuhorchak@gmail.com>
2021-06-14 10:45:47 -0400

    generate plots for all cities, all measures

--- data_pull.R
+++ data_pull.R
@@ -7,6 +7,7 @@ data_pull <- read.socrata("https://data.bts.gov/resource/keg4-3bc2.json")
 data_pull <- data_pull %>% mutate(., value = as.numeric(value))
 data_pull <- data_pull %>% mutate(., measure = as.factor(measure))
 
+
 data_pull %>% 
   filter(port_name == 'Brownsville') %>%
   ggplot(., aes(date, value, color=measure, linetype=measure)) + geom_line() +
@@ -17,7 +18,17 @@ plot_port_city <- function(data, city = 'Brownsville', ...){
   data %>% 
     filter(port_name == city) %>%
     ggplot(., aes(date, value, color=measure, linetype=measure)) + geom_line() +
-    theme(axis.text.x = element_text(angle = 90))
+    theme(axis.text.x = element_text(angle = 90)) +
+    ggtitle(label = city)
+}
+
+for (name in unique(data_pull$port_name)) {
+  
+  ggsave(filename = paste0("plot_", name, ".png"),
+         plot_port_city(data_pull, city = name),
+         width = 1024,
+         height = 768,
+         limitsize = FALSE)
 }
 
 # forecasting
@@ -27,11 +38,11 @@ brownsville_pedestrians <- data_pull %>%
   filter(port_name == 'Brownsville') %>% 
   filter(measure == 'Pedestrians') %>% 
   filter(date <= '2019-12-01') %>% 
-  select(value) %>% ts(., start= c(1996,1), end = c(2019,12), frequency = 12)
+  select(value) %>% ts(., start= c(1996,1), end = c(2020,12), frequency = 12)
 
-fit_HW <- HoltWinters(brownsville_pedestrians, beta=FALSE, gamma=FALSE)
+fit_HW <- HoltWinters((brownsville_pedestrians_diff), beta=FALSE, gamma=FALSE)
 # fit_auto_ARIMA <- auto.arima(brownsville_pedestrians)
-# fit_arima <- Arima(brownsville_pedestrians, order =c(2,1,1), seasonal = c(0,0,2))
+# fit_arima <- Arima(brownsville_pedestrians_diff, order =c(2,1,0))
 plot(fit_HW)
 forecast_HW <- forecast(fit_HW, 3)
 plot(forecast_HW)
